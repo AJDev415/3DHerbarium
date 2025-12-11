74eca37ff6ac4132f748f5d65928c20a6889dcda
AJ <abealum@proton.me>
2025-12-10 18:21:47 -0800

    Patching contributions for launch

--- src/app/contribute/page.tsx
+++ src/app/contribute/page.tsx
@@ -1,5 +1,4 @@
 /**
- * @file /app/contribute/page.tsx
  * @fileoverview page containing information on how to contribute to the 3D Digital Herbarium project.
  */
 
@@ -9,11 +8,13 @@ import Footer from '@/components/Shared/Foot'
 
 const Contribute = () => {
   return <>
-      <Header headerTitle="contribute" pageRoute="collections" />
-      <div className="min-h-[calc(100vh-177px)] h-full p-8">
-        <ContributionFormWrapper />
-      </div>
-      <Footer />
-    </>
+    <Header headerTitle="contribute" pageRoute="collections" />
+    <div className="min-h-[calc(100vh-177px)] h-full p-8">
+      {/* <ContributionFormWrapper /> */}
+      <p>If you would like to contribute to the 3D Digital Herbarium project, firstly, thank you for your interest!</p>
+      <p>We will be working on a fundraiser soon, and will also be accepting donations here on the site. Please follow our socials to find out more!</p>
+    </div>
+    <Footer />
+  </>
 }
 export default Contribute
