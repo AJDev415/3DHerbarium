2d3f7616a4c70a427f59b85863078b02427b4447
AJ <abealum@proton.me>
2025-12-10 18:11:08 -0800

    Removed TS errors about ignoring build errors

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {reactStrictMode: false}
 module.exports = {
     reactStrictMode: false,
     typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
         ignoreBuildErrors: true,
       },
       images: {
