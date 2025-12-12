f583a7d2137f89d97686d859ae64962abb5f5025
AJ <abealum@proton.me>
2025-12-11 23:58:19 -0800

    next update patch

--- src/components/Home/Client.tsx
+++ src/components/Home/Client.tsx
@@ -1,25 +1,22 @@
 /**
- * @file src/components/Home/Client.tsx
- * 
  * @fileoverview home page client
  */
 'use client'
 
-// Typical imports
 import { isMobileOrTablet } from '@/functions/client/utils/isMobile'
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
-// Default imports
 import dynamic from 'next/dynamic'
 
-// Dynamic imports
 const HomeModel = dynamic(() => import('@/components/Home/model'), { ssr: false })
 
-// Main JSX
 export default function HomePageClient() {
 
     const router = useRouter()
     if (isMobileOrTablet()) router.push('/collections/search')
+
+    useEffect(() => { if (isMobileOrTablet()) router.push('/login') }, [router])
 
     return <HomeModel />
 }
